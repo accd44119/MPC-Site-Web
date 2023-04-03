5b0aa1240b7a39866f9cd7c6eaa6d08a1df746e5
accd44119 <armand.cocaud@free.fr>
2023-04-03 17:02:31 +0200

    creation HTML CSS JS pour telechargent avec fetch

--- ZoneProtegee/Telechargement.php
+++ ZoneProtegee/Telechargement.php
@@ -19,48 +19,20 @@
 	?>
 
 	<section>
-		<?php
-			echo "<p> creation d'une archive avecc les fichiers de l'album 0000/album_erreur/photos/.</p>";
+		<div class="wrapper">
+		  <header>
+			<h1>File Downloader</h1>
+			<p>Paste url of image, video, or pdf to download. This tool is made with vanilla javascript.</p>
+		  </header>
+		  <form action="#">
+			<input type="url" placeholder="Paste file url" required>
+			<button>Download File</button>
+		  </form>
+		</div>
 
-			// Chemin des fichiers à ajouter dans l'archive
-			$RepFile=$Albums_rep . '0000/album_erreur/photos/';
-			$files_to_zip = array( $RepFile . 'xxxxxx1.jpg', $RepFile . 'xxxxxx2.jpg');
+    <script src="DownloadFile.js"></script>
 
-			// Nom de l'archive à créer
-			$zipname = $Upload_rep . 'album_erreur_archive.zip';
 
-			// Créer une nouvelle archive
-			echo "<p> Creation et ouverture de l'archive" . $zipname . ".</p>";
-			$zip = zip_open($zipname, ZIPARCHIVE::CREATE);
-			if($zip)
-			{
-				echo "<p> l'archive" . $zipname . " est ouverte OK.</p>";
-				// Parcourir la liste des fichiers à ajouter
-				foreach ($files_to_zip as $file) 
-				{
-					echo "<p> ajout fichier ". $file ." a l'archive " . $zipname . ".</p>";
-					// Ouvrir le fichier en lecture seule
-					$fp = fopen($file, 'r');
-
-					// Ajouter le fichier à l'archive
-					zip_add($zip, $file, $fp);
-
-					// Fermer le fichier
-					fclose($fp);
-				}
-
-				// Fermer l'archive
-				zip_close($zip);
-				echo "<p> fermeture de l'archive" . $zipname . " .</p>";
-			}
-			else
-			{
-				echo "<p> l'archive" . $zipname . " n'est pas crer Erreur: ". $zip. " .</p>";
-			}
-			// Afficher un message de succès
-			echo "<p> L'archive $zipname a été créée avec succès.</p>";
-
-		?>
 	</section>
 
 
