14cd76583975d31f655ac8094bf5bf4ed72c8ddd
accd44119 <armand.cocaud@free.fr>
2023-04-11 15:22:38 +0200

    creation telechargement album en cour.

--- ZoneProtegee/DownloadFile.js
+++ ZoneProtegee/DownloadFile.js
@@ -3,9 +3,21 @@ downloadBtn = document.querySelector("button");
 
 downloadBtn.addEventListener("click", e => {
     e.preventDefault();
-    downloadBtn.innerText = "Downloading file...";
-    fetchFile(fileInput.value);
+    downloadBtn.innerText = "Telechargement fichiers...";
+    fetchDirectory(fileInput.value);
+    //fetchFile(fileInput.value);
 });
+
+function fetchDirectory(url) {
+    fetch(url).then(res => {
+            console.log(res);
+            downloadBtn.innerText = "Récuperation liste photos Album...";
+       })
+    .catch(() => {
+        alert("Erreur sur recuperation liste photos Album!");
+        downloadBtn.innerText = "Téléchargement Album";
+    });
+}
 
 function fetchFile(url) {
     fetch(url).then(res => res.blob()).then(file => {
@@ -15,11 +27,11 @@ function fetchFile(url) {
         aTag.download = url.replace(/^.*[\\\/]/, '');
         document.body.appendChild(aTag);
         aTag.click();
-        downloadBtn.innerText = "Download File";
+        downloadBtn.innerText = "Téléchargement Photo....";
         URL.revokeObjectURL(tempUrl);
         aTag.remove();
     }).catch(() => {
         alert("Failed to download file!");
-        downloadBtn.innerText = "Download File";
+        downloadBtn.innerText = "Télécharger Album";
     });
 }
--- ZoneProtegee/Telechargement.php
+++ ZoneProtegee/Telechargement.php
@@ -19,14 +19,53 @@
 	?>
 
 	<section>
-		<div class="wrapper">
-			<h2>Telechargement d'un fichier</h2>
-			<p>Collé l'url du fichier a télécharger.</p>
-		  <form action="#">
-			<input type="url" placeholder="Paste file url" required>
-			<button>Download File</button>
-		  </form>
-		</div>
+		<?php
+			if(isset($_GET['Annee'])){
+				$YearDisplay = strip_tags($_GET['Annee']);
+			}
+			else{
+				$YearDisplay = "0000";					
+			}
+			if(isset($_GET['Album'])){
+				$Album = strip_tags($_GET['Album']);
+			}
+			else{
+				$Album = "album_erreur";					
+			}
+			$Album_No_Space = urlencode($Album);
+			//$TelechargerAlbum = $Protect_rep . 'Telechargement.php?Annee='. $YearDisplay .'&Album='. $Album_No_Space  ;
+			$display_Exit_Photo = $ZonePublic_rep . 'albums.php?Annee=' . $YearDisplay  ;	
+			$Url = $ZonePublic_rep . $YearDisplay . '/' . $Album . '/photos';
+			echo '<div id="Titre_Telechargement">';
+			echo '<h2> Telechargement album Année '. $YearDisplay . ' ' . $Album . '. </h2>';
+			//echo '<a href="'. $TelechargerAlbum .'"> TELECHARGEMENT Album    </a>';
+			//echo '</div>';
+			//echo '<div id="Exit_Telechargement">';
+			echo '<a href="'. $display_Exit_Photo .'">      <img src="' . $image_rep .'fleche-retour-rouge.png" title="Afficher album" alt="Afficher album"></a>';
+			echo '</div>';
+			echo '<div id="Telechargement" class="wrapper">';
+			// Recuperation de la liste des photos du repertoire photos
+			//$ListePhotos = scandir($Albums_rep . $YearDisplay . '/' . $Album . '/photos');
+			//$Num_Photo=0;
+			//foreach ($ListePhotos as $entryPhotos){
+				//if ($entryPhotos != "." && $entryPhotos != "..") {
+					//$vignette = $Albums_rep . $YearDisplay . '/'. $Album . '/vignettes/tn_' . $entryPhotos;
+					//$photos   = $YearDisplay . '/'. $Album . '/photos/' . $entryPhotos;
+					//$display_Photo = $ZonePublic_rep . 'display_photos.php?Photo='. $Num_Photo .'&Annee='. $YearDisplay .'&Album='. $Album_No_Space  ;
+					//echo '<div class="Vignette_Album">';
+					//echo '<a href="'. $display_Photo .'" class="Choix_Photos"> <img src="' . $vignette .'" alt="'. $vignette. '"> </a>';
+					//echo '</div>';
+					//}					
+					//$Num_Photo++;				
+				//}			  			
+			echo "<h3>Telechargement de l'album</h3>";
+			echo '<form action="#">';
+			echo '	<input type="url" value = "' . $Url. '" placeholder="Paste file url" required>';
+			echo '	<button>Téléchargement Album</button>';
+			//echo '	<button>Abandon</button>';
+			echo '</form>';
+			echo '</div>';				
+		?>			  			
 
 		<script src="DownloadFile.js"></script>
 
